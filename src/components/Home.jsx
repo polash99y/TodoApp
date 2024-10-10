0bf5bfa16e559918b7412c4bd84715bf3356a08d
polash99y <mdp01864@gmail.com>
2024-10-10 09:10:37 +0600

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+import {v4 as uuidv4} from "uuid"
+import Todos from './Todos'
+import style from './Home.module.css'
+import NewTodo from './NewTodo'
+
+
+const Home = () => {
+
+  const [todos, setTodos] = useState([])
+
+  const handleTodo = (todo)=>{
+    setTodos((prevTodos)=>{
+      return [...prevTodos, {id:uuidv4(),todo}]
+    })
+  }
+
+  const handleRemoveTodo = (id)=>{
+    
+    setTodos((prevTodos)=>{
+      const filtreredTodos = prevTodos.filter((todo)=> todo.id !== id);
+      return filtreredTodos
+    })
+  }
+
+  return (
+    <div className={style.container}>
+      <h1 style={{color:"white"}}>Todo App</h1>
+      <NewTodo onAddTodo={handleTodo} />
+      <Todos todos={todos} onRemoveTodo={handleRemoveTodo} />
+    </div>
+  )
+}
+
+export default Home
